0ae339242dd60f73890c2d137dbc886d304fffa8
chinki kumari singh <singhchinkikumari.com>
2022-06-16 21:12:20 +0530

    [Chinki] Ability to load the indian states Census Information

---
+++ IndianStateCansusAnalyser/CensusAnalyserTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CensusAnalyserTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ IndianStateCansusAnalyser/IndianStateCansusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndianStateCansusAnalyser.POCO;
+using Newtonsoft.Json;
+
+namespace IndianStateCansusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,US,BRAZIL
+        }
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvfilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvfilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
