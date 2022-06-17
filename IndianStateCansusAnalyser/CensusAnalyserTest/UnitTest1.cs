691b63e5468eff4b75d9c55cc357e2e1104592b3
chinki kumari singh <singhchinkikumari.com>
2022-06-17 14:08:42 +0530

    [Chinki] If the file Incorrecct Returns a custom Exception

--- IndianStateCansusAnalyser/CensusAnalyserTest/UnitTest1.cs
+++ IndianStateCansusAnalyser/CensusAnalyserTest/UnitTest1.cs
@@ -12,15 +12,16 @@ namespace CensusAnalyserTest
     {
         static string indianStateCensusHeaders = "State,Population,AreaInSqkn,DensityPerSqkn";
         static string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
-        static string indianStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string wrongHeaderIndianCensusPath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string delimiterIndianStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string wrongindanStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string wrongindianStateCensusFileType = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string indianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string wrongIndianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string delimiterIndianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
-        static string wrongHeaderStateCoderFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles";
+        static string indianStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\StateCensusData.csv";
+        static string wrongHeaderIndianCensusPath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\wrongHeaderIndianCensusPath.csv";
+        static string delimiterIndianStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\delimeterIndianStateCensus.csv";
+        static string wrongindanStateCensusFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\";
+        static string wrongindianStateCensusFileType = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\wrongIndianStateCensusFileType.txt";
+        static string indianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\StateCode.csv";
+        static string wrongIndianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\wrongIndianStatrCodeFilePath.txt";
+        static string delimiterIndianStateCodeFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\delimeterIndianStateCode.csv";
+        static string wrongHeaderStateCoderFilePath = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\wrongHeaderStateCode.csv";
+        static string wrongIndianStateCoderFileType = @"D:\BridgeLabzFellowship\CensusAnalayser\IndianStateCansusAnalyser\CensusAnalyserTest\CSVFiles\wrongIndianStateCodeFileType.txt";
 
         IndianStateCansusAnalyser.CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
@@ -35,8 +36,7 @@ namespace CensusAnalyserTest
             stateRecord = new Dictionary<string, CensusDTO>();
         }
         
-        /// TC-1.1-Givens the indian census data file when readed should return census data count.
-        
+       
         [Test]
         public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
         {
@@ -46,16 +46,24 @@ namespace CensusAnalyserTest
             Assert.AreEqual(37, stateRecord.Count);
         }
 
-        ///TC-1.2- Givens the wrong indian census data file when readed should return custom exception.
+        
         [Test]
         public void GivenWrongIndianCensusDataFile_WhenReaded_ShouldReturnCustomException()
         {
-            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongindianStateCensusFileType, indianStateCensusHeaders));
-            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongindanStateCensusFilePath, indianStateCodeHeaders));
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongindanStateCensusFilePath ,indianStateCensusHeaders));
+            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongindanStateCensusFilePath ,indianStateCodeHeaders));
             Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, censusException.eType);
             Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, stateException.eType);
         }
 
+        [Test]
+        public void GivenWrongIndianCensusDataFileType_WhenReaded_ShouldReturnCustomException()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongindianStateCensusFileType, indianStateCensusHeaders));
+            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongIndianStateCodeFilePath, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, censusException.eType);
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, stateException.eType);
+        }
 
     }
 }
